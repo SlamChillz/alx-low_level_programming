e7b3edd1200e3ce5b691a467c2669429c9732c0d
slamchillz <mendyslam@gmail.com>
2022-04-01 22:55:25 +0000

    Minimum number

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -8,11 +8,13 @@
  */
 int main(int argc, char *argv[])
 {
-	int i = 1;
-	(void) argc;
+	int i;
 
-	while (argv[i])
-		printf("%s\n", argv[i++]);
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+
 	return (0);
 }
 
