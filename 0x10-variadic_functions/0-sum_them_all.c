fefeea927c7e3ec6c7dd5f6c42ded58febbc4715
slamchillz <mendyslam@gmail.com>
2022-04-14 23:09:49 +0000

    Function, print all valid format arguments

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,24 +1,27 @@
 #include "variadic_functions.h"
 
 /**
- * sum_them_all - sum parameters of a function
- * @n: integer varible
- * @...: list of integer variables
+ * sum_them_all - returns the sum of all its parameters.
+ * @n: amount of the arguments.
  *
- * Return: integer, sum or 0 if n equals 0
+ * Return: sum of its parameters.
  */
 int sum_them_all(const unsigned int n, ...)
 {
-	va_list p;
-	int sum = 0;
+	va_list valist;
 	unsigned int i;
+	int sum = 0;
 
 	if (n == 0)
 		return (0);
-	va_start(p, n);
+
+	va_start(valist, n);
+
 	for (i = 0; i < n; i++)
-		sum += va_arg(p, int);
-	va_end(p);
+		sum += va_arg(valist, int);
+
+	va_end(valist);
+
 	return (sum);
 }
 
--- 0x10-variadic_functions/README.md
+++ 0x10-variadic_functions/README.md
@@ -28,3 +28,13 @@ The learning objective of this projects are:
 - If one of the string is NULL, print (nil) instead
 - Print a new line at the end of your function
 
+### 3. To be is a to be the value of a variable
+[3-print_all.c](./3-print_all.c) a function that prints anything.
+- Prototype: `void print_all(const char * const format, ...)`
+- where format is a list of types of arguments passed to the function
+	- c: char
+	- i: integer
+	- f: float
+	- s: char * (if the string is NULL, print (nil) instead
+	- any other char should be ignored
+
