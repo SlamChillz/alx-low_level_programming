15caceeea41f7ab0f9f277f7e4f8d20464b65d42
slamchillz <mendyslam@gmail.com>
2022-05-02 21:27:09 +0000

    A program to copy a file content into another file

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,11 +1,13 @@
 #ifndef FILE_H
 #define FILE_H
 
+#include <stdio.h>
+#include <stdlib.h>
+
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <unistd.h>
-#include <stdlib.h>
 
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
