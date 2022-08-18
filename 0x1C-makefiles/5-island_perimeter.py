d4c94ce2683fb645c7d4c3371296018416579db1
Slamchillz <mendyslam@gmail.com>
2022-08-18 09:14:58 +0000

    Algorithm meets intellisence

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -11,14 +11,13 @@ def island_perimeter(grid):
     """
     length = len(grid)
     breath = len(grid[0])
-    rightBottom = 0
-    cellSize = 0
+    perimeter = 0
     for x in range(length):
         for y in range(breath):
             if grid[x][y] == 1:
-                cellSize += 1
+                perimeter += 4
                 if (y < breath - 1 and grid[x][y + 1] == 1):
-                    rightBottom += 1
+                    perimeter -= 2
                 if (x < length - 1 and grid[x + 1][y] == 1):
-                    rightBottom += 1
-    return (cellSize * 4 - rightBottom * 2)
+                    perimeter -= 2
+    return perimeter
